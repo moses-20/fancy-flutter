363eea4649a7c61422b65b0729ff68e18a6657b1
moses-20 <mosesgameli20@gmail.com>
2021-01-20 13:38:39 +0000

    reset main.dart file for multiple routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/list_view.dart';
+import 'pages/flu_tabs.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Raja Yogan',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: _Home(),
-    );
-  }
+  runApp(MaterialApp(
+    initialRoute: '/flutabs',
+    routes: {
+      '/': (context) => _Home(),
+      '/flutabs': (context) => Flutabs(),
+    },
+  ));
 }
 
 class _Home extends StatelessWidget {
